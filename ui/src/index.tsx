1344e7808b51fb57e9eb240e8c8aa0018e81d064
joaotomaspinheiro <joaotomasp@gmail.com>
2024-09-09 00:20:07 +0100

    feat: support itch io iframe

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,17 +5,14 @@ import { Router, Route } from '@solidjs/router';
 import './index.css';
 import Home from './pages/home/home';
 import Game from './pages/game/game';
-import NotFound from './pages/not-found/not-found';
 
 const root = document.getElementById('root');
 
 render(
 	() => (
 		<Router>
-			<Route path="/" component={Home} />
-			<Route path="/index.html" component={Home} />
+			<Route path="*" component={Home} />
 			<Route path="/play" component={Game} />
-			<Route path="*404" component={NotFound} />
 		</Router>
 	),
 	root!,
