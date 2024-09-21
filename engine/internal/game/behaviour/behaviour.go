36d71f7aa9a323ed9bba06f9ae0aeb1d206dd163
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-09-21 18:11:26 +0100

    feat: add camera animation transition (#44)
    Refs: closes #43 
    
    ## Summary
    
    Add an animation transition to the camera when changing its position.
    The animation uses a easing out sine function.
    
    ## Changes
    
    - Update camera position to centralize the map
    - Add camera configuration for the transition speed
    - Update camera controller to consider the transition when changing its
    position

--- engine/internal/game/behaviour/behaviour.go
+++ engine/internal/game/behaviour/behaviour.go
@@ -1,6 +1,17 @@
 package behaviour
 
+import "math"
+
 const (
 	// Epsilon defines the epsilon used in the behaviours.
 	Epsilon = 0.1
 )
+
+// easeOutSine calculates the easing out sine function for a given amount t.
+// The parameter t represents the absolute progress of the animation in the bounds of 0 (beginning of the animation) and
+// 1 (end of animation).
+//
+// It is used to create a smooth animation transition.
+func easeOutSine(t float64) float64 {
+	return math.Sin(t * math.Pi / 2)
+}
--- engine/internal/game/behaviour/cameracontroller.go
+++ engine/internal/game/behaviour/cameracontroller.go
@@ -3,6 +3,7 @@ package behaviour
 import (
 	"github.com/goofr-group/game-engine/pkg/engine"
 	"github.com/goofr-group/game-engine/pkg/rendering"
+	"github.com/goofr-group/go-math/mathf"
 	"github.com/goofr-group/go-math/vector2"
 	"github.com/goofr-group/physics-engine/pkg/game"
 
@@ -15,14 +16,29 @@ type CameraController struct {
 
 	playerObject    *game.Object    // Defines the player object.
 	initialPosition vector2.Vector2 // Defines the camera initial position.
+
+	previousPosition vector2.Vector2 // Defines the previous position of the camera.
+	currentPosition  vector2.Vector2 // Defines the current position of the camera. It represents the target position of the current level.
+
+	transition      float64 // Defines the current amount, in the range [0; 1], that has been transitioned from the previousPosition to the currentPosition.
+	transitionSpeed float64 // Defines the speed of the animation transition.
 }
 
 // NewCameraController returns a new camera controller behaviour.
 func NewCameraController(
 	camera *rendering.Camera,
+	cameraTransitionSpeed float64,
 ) CameraController {
+	// Check that the camera transition speed is valid.
+	if cameraTransitionSpeed <= 0 {
+		// Invalid speed, defaults to 1.
+		cameraTransitionSpeed = 1
+	}
+
 	return CameraController{
 		camera: camera,
+
+		transitionSpeed: cameraTransitionSpeed,
 	}
 }
 
@@ -37,10 +53,14 @@ func (b *CameraController) Start(e *engine.Engine) error {
 	// Save the camera initial position.
 	b.initialPosition = b.camera.Position
 
+	// Initialize the previous and current positions.
+	b.previousPosition = b.initialPosition
+	b.currentPosition = b.initialPosition
+
 	return nil
 }
 
-func (b *CameraController) Update(_ *engine.Engine) error {
+func (b *CameraController) Update(e *engine.Engine) error {
 	// Check if the player object is accessible.
 	if b.playerObject == nil {
 		return nil
@@ -54,7 +74,33 @@ func (b *CameraController) Update(_ *engine.Engine) error {
 
 	// Compute the camera position based on the player minimum bound.
 	level := int((playerBounds.Min.Y - b.initialPosition.Y + b.camera.PixelHeight*0.5) / b.camera.PixelHeight)
-	b.camera.Position.Y = b.camera.PixelHeight*float64(level) + b.initialPosition.Y
+
+	// newPosition represents the new position of the camera considering the current level of the player.
+	newPosition := vector2.Vector2{
+		X: b.camera.Position.X,
+		Y: b.camera.PixelHeight*float64(level) + b.initialPosition.Y,
+	}
+
+	// Check whether the camera position has changed.
+	if newPosition != b.currentPosition {
+		// Update the previous and current positions.
+		b.previousPosition = b.camera.Position
+		b.currentPosition = newPosition
+
+		// Reset the animation transition.
+		b.transition = 0
+	}
+
+	// If the transition has been completed, avoid unnecessary computation.
+	if b.transition >= 1 {
+		return nil
+	}
+
+	// Compute the new animation transition.
+	b.transition = mathf.Clamp(b.transition+e.Time().DeltaTime*b.transitionSpeed, 0, 1)
+
+	// Apply the current transition using an easing function.
+	b.camera.Position = vector2.Lerp(b.previousPosition, b.currentPosition, easeOutSine(b.transition))
 
 	return nil
 }
