e7384a19c9c6e0f5130c0d2e641e4cbeb6317b37
joaotomaspinheiro <joaotomasp@gmail.com>
2025-02-22 20:58:12 +0000

    wip

--- ui/src/pages/play/utils/sound.ts
+++ ui/src/pages/play/utils/sound.ts
@@ -6,7 +6,7 @@ import { GameObjectSound } from '../../../domain/game-state';
 export const SOUND_SOURCES = {
 	[GameObjectSound.JUMP]: new Audio('sounds/jump.ogg'),
 	[GameObjectSound.JUMP_HOLD]: new Audio('sounds/.ogg'),
-	[GameObjectSound.KNOCK_BACK]: new Audio('sounds/.ogg'),
+	[GameObjectSound.KNOCK_BACK]: new Audio('sounds/knockBack.ogg'),
 	[GameObjectSound.LANDING]: new Audio('sounds/landing.ogg'),
 	[GameObjectSound.FALL]: new Audio('sounds/.wav'),
 };
