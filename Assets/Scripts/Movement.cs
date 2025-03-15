04019c952a9c2f583726343a66851136ed85f602
João Pinheiro <joaotomasp@gmail.com>
2025-03-15 20:30:12 +0000

    feat: add player movement (#16)
    Refs: closes #5
    
    ## Summary
    
    Add player movement behavior.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,79 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Defines the movement speed multiplier.
+    /// </summary>
+    [SerializeField] private float speed;
+
+    // Action status.
+    private bool leftActionStatus;
+    private bool jumpActionStatus;
+    private bool rightActionStatus;
+
+    // Required components.
+    private SpriteRenderer spriteRenderer;
+    private new Rigidbody2D rigidbody2D;
+    private CheckGround checkGround;
+
+    // Input actions.
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        checkGround = GetComponentInChildren<CheckGround>();
+
+        moveAction = InputSystem.actions.FindAction(GameManager.INPUT_ACTION_MOVE);
+        jumpAction = InputSystem.actions.FindAction(GameManager.INPUT_ACTION_JUMP);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Update the actions.
+        float moveValue = moveAction.ReadValue<float>();
+
+        leftActionStatus = moveValue < 0;
+        rightActionStatus = moveValue > 0;
+        jumpActionStatus = jumpAction.IsPressed();
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if the object is in contact with the ground.
+        if (!checkGround.IsTouchingGround() || rigidbody2D.linearVelocityY > Mathf.Epsilon)
+        {
+            return;
+        }
+
+        // Compute the direction to add to the object based on the left and right actions.
+        int direction = 0;
+        if (leftActionStatus)
+        {
+            // Add the left direction.
+            direction -= 1;
+            spriteRenderer.flipX = true;
+        }
+        if (rightActionStatus)
+        {
+            // Add the right direction.
+            direction += 1;
+            spriteRenderer.flipX = false;
+        }
+
+        // Reset the horizontal velocity of the object 
+        // when no movement action is performed or the jump action is being performed.
+        if (direction == 0 || jumpActionStatus)
+        {
+            rigidbody2D.linearVelocityX = 0;
+            return;
+        }
+
+        // Add the computed velocity when the movement actions are performed.
+        rigidbody2D.linearVelocityX = direction * speed * Time.fixedDeltaTime;
     }
 }
