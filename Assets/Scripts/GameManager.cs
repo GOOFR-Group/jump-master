73dee4b35d5ed4b9b46bc9c3db2a5799794d3a80
joaotomaspinheiro <joaotomasp@gmail.com>
2025-03-15 20:25:49 +0000

    feat: add player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Tag constants.
+    public const string TAG_PLATFORM = "Platform";
+
+    // Action constants.
+    public const string INPUT_ACTION_MOVE = "Move";
+    public const string INPUT_ACTION_JUMP = "Jump";
+}
