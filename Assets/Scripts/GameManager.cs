6bf735dc2280e56632c0b191c6b37af91992a722
João Pinheiro <joaotomasp@gmail.com>
2025-03-16 23:53:33 +0000

    feat: set up UI (#20)
    Refs: closes #2
    
    ## Summary
    
    Set up UI.
    
    ---------
    
    Co-authored-by: Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Scene constants.
+    public const string SCENE_MAIN = "MainScene";
+
     // Tag constants.
     public const string TAG_PLATFORM = "Platform";
 
     // Action constants.
-    public const string INPUT_ACTION_MOVE = "Move";
-    public const string INPUT_ACTION_JUMP = "Jump";
+    public const string INPUT_ACTION_MAP_PLAYER = "Player";
+    public const string INPUT_ACTION_MAP_MENU = "Menu";
+
+    public const string INPUT_ACTION_PLAYER_MOVE = "Move";
+    public const string INPUT_ACTION_PLAYER_JUMP = "Jump";
+
+    public const string INPUT_ACTION_MENU_LEFT = "Left";
+    public const string INPUT_ACTION_MENU_RIGHT = "Right";
+    public const string INPUT_ACTION_MENU_JUMP = "Jump";
+
+    /// <summary>
+    /// Toggles the time scale to pause or resume the game.
+    /// </summary>
+    public static void ToggleTimeScale()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
 }
